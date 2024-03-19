0d7f0d6d1576fba6e9a9f42511c6989de57164f1
Gonzalo Quispe <gquispehu@externo.auna.pe>
2024-03-19 12:40:44 -0500

    Feat: Add translate

--- script.js
+++ script.js
@@ -3,38 +3,46 @@ function generarQR() {
     var piso = document.getElementById('piso').value;
     var clinica = document.getElementById('clinica').value;
     
-    // Definir los valores correspondientes según el ambiente seleccionado
-    var apn, ibi, isi;
+    var apn, ibi, isi, link;
     switch (ambiente) {
       case 'dev':
+        link = 'appinnovauat.page.link';
+        web = 'https://dev-mi.auna.pe/';
         apn = 'com.auna.int.appinnova';
         ibi = 'pe.com.auna.patientportal.develop';
         isi = '6476938155';
         break;
       case 'prd':
+        link = 'auna.page.link';
+        web = 'https://mi.auna.pe/';
         apn = 'pe.com.auna.patientportal';
         ibi = 'pe.com.mdp.aunaapp';
         isi = '1188108872';
         break;
       case 'qas':
+        link = 'appinnovauat.page.link';
+        web = 'https://stg-mi.auna.pe/';
         apn = 'com.auna.test.appinnova';
         ibi = 'pe.com.auna.patientportal.stage';
         isi = '6477191730';
         break;
       case 'uat':
+        link = 'appinnovauat.page.link';
+        web = 'https://uat-mi.auna.pe/';
         apn = 'com.auna.uat.appinnova';
         ibi = 'pe.com.auna.patientportal.uat';
         isi = '6477191757';
         break;
       default:
         // Por defecto, usar valores de producción
+        link = 'auna.page.link';
+        web = 'https://mi.auna.pe/';
         apn = 'pe.com.auna.patientportal';
         ibi = 'pe.com.mdp.aunaapp';
         isi = '1188108872';
     }
   
-    // Generar la URL completa con los valores proporcionados
-    var urlBase = 'https://auna.page.link/?link=https://mi.auna.pe/?piso=' + piso + '%26clinic=' + clinica + '&apn=' + apn + '&ibi=' + ibi + '&isi=' + isi + '&efr=0';
+    var urlBase = 'https://'+link+'/?link='+web+'?piso=' + piso + '%26clinic=' + clinica + '&apn=' + apn + '&ibi=' + ibi + '&isi=' + isi + '&efr=1';
   
     // Generar el código QR
     var qrContainer = document.getElementById('qrCode');
@@ -44,5 +52,7 @@ function generarQR() {
       width: 150,
       height: 150
     });
+
+    var translate = document.getElementById('translate');
+    translate.innerHTML = urlBase;
   }
-  
